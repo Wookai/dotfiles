7d3d7da154753d015c71f93b9694f0e636bdc5bd
Vincent Etter <vincent.etter@gmail.com>
2013-03-11 16:36:10 +0100

    Added removal of existing files before creating symlinks

--- createLinks.sh
+++ createLinks.sh
@@ -6,6 +6,9 @@ DOTDIR=`pwd`
 # go to home dir
 cd
 
+# remove files if they exist
+rm -f .bash_aliases .bashrc .screenrc .vimrc
+
 # create symlinks
 ln -s "$DOTDIR/bash_aliases" .bash_aliases
 ln -s "$DOTDIR/bashrc" .bashrc
