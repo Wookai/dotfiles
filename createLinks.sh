231c1c019ec8a0bca80b7d2d871958bf5ab3ebf6
Vincent Etter <vincent.etter@gmail.com>
2013-03-11 15:23:20 +0100

    Removed unnecessary ~/ in symlinks creation

--- createLinks.sh
+++ createLinks.sh
@@ -8,6 +8,6 @@ cd
 
 # create symlinks
 ln -s "$DOTDIR/bash_aliases" .bash_aliases
-ln -s "$DOTDIR/bashrc" ~/.bashrc
-ln -s "$DOTDIR/screenrc" ~/.screenrc
-ln -s "$DOTDIR/vimrc" ~/.vimrc
+ln -s "$DOTDIR/bashrc" .bashrc
+ln -s "$DOTDIR/screenrc" .screenrc
+ln -s "$DOTDIR/vimrc" .vimrc
