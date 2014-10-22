3805231d631b0ac026d00c38666b35e6a8820830
Vincent Etter <vincent.etter@gmail.com>
2014-10-22 10:27:51 +0200

    Added git config to dot files

--- createLinks.sh
+++ createLinks.sh
@@ -7,10 +7,11 @@ DOTDIR=`pwd`
 cd
 
 # remove files if they exist
-rm -f .bash_aliases .bashrc .screenrc .vimrc
+rm -f .bash_aliases .bashrc .screenrc .vimrc .gitconfig
 
 # create symlinks
 ln -s "$DOTDIR/bash_aliases" .bash_aliases
 ln -s "$DOTDIR/bashrc" .bashrc
 ln -s "$DOTDIR/screenrc" .screenrc
 ln -s "$DOTDIR/vimrc" .vimrc
+ln -s "$DOTDIR/gitconfig" .gitconfig
